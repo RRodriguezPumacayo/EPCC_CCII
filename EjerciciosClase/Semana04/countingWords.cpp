3454f132758711ac73cc61a69b21e6ba00f951b8
RickyMaru <rrodriguezp@unsa.edu.pe>
2022-05-05 16:33:30 -0500

    logrado

--- EjerciciosClase/Semana04/countingWords.cpp
+++ EjerciciosClase/Semana04/countingWords.cpp
@@ -3,16 +3,49 @@
 • A word is a string of non space characters separated by white space
 */
 
-#include<cerrno>
-#include<algorithm>
-#include<string>
+/*
+Con la libreria map podemos crear arrays asociativos, estos tienen elementos que van asociados por una clave
+a diferencia de un array tradicional donde cada valor es indexado numericamente.
+*/
 #include<iostream>
-#include<iterator>
+#include<map>
+#include<string>
 #include<fstream>
 
 int main(){
-    std::string name{"data.in"};
-    std::ifstream in{name};
-    if(not in)
-        std::cerr<<name<<": "<<std::generic_category().message(errno)<<'\n';
+    //std::ifstream archivo; //Para la tectura de un archivo
+    //archivo.open("tarea.txt",std::ios::out); //Para la tectura de un archivo
+
+    std::map<std::string, int> counts{};//CLAVE(key) string, valor int; the name of the array is counts
+    std::string word{};
+    int i=0;
+    std::cout<<"Input 5 times the same word\n";
+    while(std::cin>>word){
+        ++counts[word];
+        std::cout<<"La palabra "<<word<<" se repite "<<counts[word]<<" veces\n";
+        if(counts[word]==5){
+            break;
+        }
+    }
+    //TODO: Imprimir el resultado
+    //Para imprimir los valores usamos "iterator",
+    std::map<std::string, int>::iterator rator;//map<key,valor>::iterador name
+
+    for(rator = counts.begin();rator != counts.end();rator++){
+        //rator->first nos da la clave(key)
+        //rator->second nos da el valor(key)
+        std::cout<<"Clave = "<<rator->first<<": Valor = "<<rator->second<<" La palabra ("<<rator->first<<") se repite "<<rator->second<<" veces."<<std::endl;
+    }
+
+    /*
+    //LEER UN ARCHIVO
+    while(!archivo.eof()){
+        getline(archivo,word);
+        ++counts[word];
+        std::cout<<"La palabra "<<word<<" se repite "<<counts[word]<<" veces\n";
+    }
+    */
+
+
+    return 0;
 }
