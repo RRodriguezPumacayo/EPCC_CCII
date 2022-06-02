d16fd1496507bf963c1820bc43a1c6159afbc5e2
RickyMaru <rrodriguezp@unsa.edu.pe>
2022-06-02 17:08:58 -0500

    complete

---
+++ Proyecto/JuegoBomberman/JuegoBomberman/Controladora.h
@@ -0,0 +1,27 @@
+#ifndef _CONTROLADORA_H_
+#define _CONTROLADORA_H_
+#include"Escenario.h"
+
+class CControladora
+{
+	public:
+		CControladora() {
+			oEscenario = new CEscenario();
+		}
+		~CControladora() {}
+
+		void cambiarNivel()
+		{
+			oEscenario->generarMatriz();
+		}
+
+		void dibujar(Graphics^ g, Bitmap^bmpBase, Bitmap^ bmpSolido, Bitmap^ bmpDestruible) 
+		{
+			oEscenario->pintarBase(g,bmpBase);
+			oEscenario->pintarMatriz(g, bmpSolido, bmpDestruible);
+		}
+	private:
+		CEscenario* oEscenario;
+};
+
+#endif
---
+++ Proyecto/JuegoBomberman/JuegoBomberman/Juego.h
@@ -0,0 +1,96 @@
+#pragma once
+#include"Controladora.h"
+
+
+namespace JuegoBomberman {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de Juego
+	/// </summary>
+	public ref class Juego : public System::Windows::Forms::Form
+	{
+	public:
+		CControladora* oControladora = new CControladora(); 
+		Bitmap^ bmpSolido = gcnew Bitmap("imagenes\\bmpSolido.png");
+		Bitmap^ bmpDestruible = gcnew Bitmap("imagenes\\bmpDestruible.png");
+		Bitmap^ bmpSuelo = gcnew Bitmap("imagenes\\bmpSuelo.png");
+		Juego(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~Juego()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Timer^ timer1;
+	protected:
+
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->SuspendLayout();
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Juego::timer1_Tick);
+			// 
+			// Juego
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(417, 426);
+			this->Name = L"Juego";
+			this->Text = L"Juego";
+			this->Load += gcnew System::EventHandler(this, &Juego::Juego_Load);
+			this->ResumeLayout(false);
+
+		}
+
+#pragma endregion
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		Graphics^ g = this->CreateGraphics();
+		BufferedGraphicsContext^ espacio = BufferedGraphicsManager::Current;
+		BufferedGraphics^ buffer = espacio->Allocate(g, this->ClientRectangle);
+
+		oControladora->dibujar(buffer->Graphics, bmpSuelo, bmpSolido, bmpDestruible);
+		buffer->Render(g);
+		delete buffer, espacio, g;
+	}
+	private: System::Void Juego_Load(System::Object^ sender, System::EventArgs^ e) {
+		oControladora->cambiarNivel();
+	}
+	};
+}
